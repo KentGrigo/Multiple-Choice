cc725855ebe0db6c679b93ef07b613604da3ce2f
Kent Grigo <KentGrigo@hotmail.com>
2021-12-26 09:04:23 +0100

    Refactor credits

--- Credit.js
+++ Credit.js
@@ -1,22 +1,24 @@
 class Credit {
+    formatListAsText(authors) {
+        if (authors.length === 1) {
+            return authors[0]
+        } else if (authors.length === 2) {
+            return authors[0] + " and " + authors[1]
+        } else {
+            const lastAuthor = authors.pop()
+            return authors.join(", ") + ", and " + lastAuthor
+        }
+    }
+
     getAuthorText() {
-        var authorText = ""
         if (typeof(metadata) === 'undefined' || metadata === null
             || metadata.authors === undefined || metadata.authors === null
             || metadata.authors.length === 0) {
-            authorText = "unknown"
+            return "unknown"
         } else {
             const authors = metadata.authors.slice()
-            if (authors.length === 1) {
-                authorText = authors[0]
-            } else if (authors.length === 2) {
-                authorText = authors[0] + " and " + authors[1]
-            } else {
-                const lastAuthor = authors.pop();
-                authorText = authors.join(", ") + ", and " + lastAuthor;
-            }
+            return this.formatListAsText(authors)
         }
-        return authorText
     }
 
     updateHtml() {
